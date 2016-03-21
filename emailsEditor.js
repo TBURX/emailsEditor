f5ea58e3e034e399ebe5f632dc0a7fdfc94abb3d
Vitaly <subbot.v@gmail.com>
2016-03-22 03:59:43 +0500

    почти готово
    Функционал есть, надо подкрутить стили и изолированный скоп.

--- emailsEditor.js
+++ emailsEditor.js
@@ -3,8 +3,8 @@ var app = angular.module("editEmailsApp", []);
 
 app.directive("emailsEditor", function() {
 	var templ = ' <div onclick="this.getElementsByTagName(\'input\')[0].focus();">'+
-        '{{eMails.toString()}}'+
-        '<input id="input" type="text" ng-model="emailsString" ng-change="change()" ng-keyup="keyUp($event)" ng-blur="blur()" placeholder="add more people...">'+
+        '<span class="email {{email.correct}}" ng-repeat="email in eMails">{{email.name}}<span class="remove" ng-click="remove($index)">&#10006;</span></span>'+
+        '<input type="text" id="email-input" ng-model="emailsString" ng-change="change()" ng-keyup="keyUp($event)" ng-blur="blur()" placeholder="add more people...">'+
         '</div>';
     return {
         template : templ/*,
@@ -22,7 +22,7 @@ app.controller("controller", function($scope){
 		alert("e-mails count is: " + $scope.eMails.length);
 	};
     $scope.addEmails = function(){
-        var charSet="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        var charSet="qwertyuiopasdfghjklzxcvbnm";
         var sample="";
         var len=Math.floor(Math.random()*8+3);
         for(i=0; i<len; i++)
@@ -59,6 +59,9 @@ app.controller("controller", function($scope){
     $scope.blur = function(){
             addEmails($scope);
     };
+    $scope.remove = function($index){
+        $scope.eMails.splice($index,1);
+    }
 });
 
 function addEmails($scope){
@@ -67,11 +70,32 @@ function addEmails($scope){
     while(i<tmpEmails.length)
         {
             tmpEmails[i]=tmpEmails[i].trim();
-            if(tmpEmails[i]=="")
+            if(tmpEmails[i]==""||
+               findIndexByName($scope.eMails,tmpEmails[i])>=0
+              )
                 tmpEmails.splice(i,1);
             else
                 i++;
         }
-    $scope.eMails = $scope.eMails.concat(tmpEmails);
+    
+    var emailRegExp = /^[a-z]+[a-z0-9._-]+@[a-z]+\.[a-z.]{2,5}$/;
+    for(i=0;i<tmpEmails.length;i++)
+        {
+            if(emailRegExp.test(tmpEmails[i]))
+                $scope.eMails.push({name: tmpEmails[i], correct:"correct"});
+            else
+                $scope.eMails.push({name: tmpEmails[i], correct:"wrong"});
+        }
+    //$scope.eMails = $scope.eMails.concat(tmpEmails);
     $scope.emailsString="";
+}
+
+function findIndexByName(source, name){
+    for(i=0;i<source.length;i++){
+        if(source[i].name === name)
+            {
+                return i;
+            }
+    }
+    return -1;
 }
