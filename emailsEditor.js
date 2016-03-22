f35a562f25f5142a2d1b63e7ca52a5d6b37891b0
Vitaly <subbot.v@gmail.com>
2016-03-22 18:09:16 +0500

    изолированный скоп
    робит

--- emailsEditor.js
+++ emailsEditor.js
@@ -1,65 +1,68 @@
-
 var app = angular.module("editEmailsApp", []);
 
 app.directive("emailsEditor", function() {
     return {
-        restrict: 'E',
+        restrict: 'E'
+        ,
         link: function($scope, $elem, $attrs){
-            console.log("Hello, i'm em-ed directive");
-        },
+            console.log("Hello, i'm emails-edit directive");
+            $scope.emailsString="";
+            $scope.eMails = [];
+            $scope.getEmailsCount = function(){
+                alert("e-mails count is: " + $scope.eMails.length);
+            };
+            $scope.addEmail = function(){
+                var charSet="qwertyuiopasdfghjklzxcvbnm";
+                var sample="";
+                var len=Math.floor(Math.random()*8+3);
+                for(i=0; i<len; i++)
+                    {
+                        sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                    }
+                sample+="@";
+                len=Math.floor(Math.random()*8+3);
+                for(i=0; i<len; i++)
+                    {
+                        sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                    }
+                sample+=".";
+                len=Math.floor(Math.random()*2+2);
+                for(i=0; i<len; i++)
+                    {
+                        sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
+                    }
+                $scope.emailsString=sample;
+                addEmails($scope);        
+            };
+            $scope.change = function(){
+                if($scope.emailsString.indexOf(",")>=0 )
+                {
+                    addEmails($scope);
+                }
+            };
+            $scope.keyUp = function($event){
+                if($event.keyCode==13)
+                {
+                    addEmails($scope);
+                }
+            };
+            $scope.blur = function(){
+                    addEmails($scope);
+            };
+            $scope.remove = function($index){
+                $scope.eMails.splice($index,1);
+            };
+            $scope.focus = function(){
+                ($elem)[0].getElementsByTagName("input")[0].focus();
+            };
+        }
+        ,
         templateUrl : "emailsEditor.html"
-    };
-});
-
-app.controller("controller", function($scope){
-    $scope.emailsString="";
-    $scope.eMails = [];
-	$scope.getEmailsCount = function(){
-		alert("e-mails count is: " + $scope.eMails.length);
-	};
-    $scope.addEmails = function(){
-        var charSet="qwertyuiopasdfghjklzxcvbnm";
-        var sample="";
-        var len=Math.floor(Math.random()*8+3);
-        for(i=0; i<len; i++)
-            {
-                sample+=charSet[Math.floor(Math.random()*charSet.length)];
-            }
-        sample+="@";
-        len=Math.floor(Math.random()*8+3);
-        for(i=0; i<len; i++)
-            {
-                sample+=charSet[Math.floor(Math.random()*charSet.length)];
-            }
-        sample+=".";
-        len=Math.floor(Math.random()*2+2);
-        for(i=0; i<len; i++)
-            {
-                sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
-            }
-        $scope.emailsString=sample;
-        addEmails($scope);        
-    };
-    $scope.change = function(){
-        if($scope.emailsString.indexOf(",")>=0 )
-        {
-            addEmails($scope);
+        ,
+        scope: {
+            addEmail: "=addEmail",
+            getEmailsCount: "=getEmailsCount"
         }
-    };
-    $scope.keyUp = function($event){
-        if($event.keyCode==13)
-        {
-            addEmails($scope);
-        }
-    };
-    $scope.blur = function(){
-            addEmails($scope);
-    };
-    $scope.remove = function($index){
-        $scope.eMails.splice($index,1);
-    };
-    $scope.focus = function(){
-        document.getElementById("email-input").focus();
     };
 });
 
